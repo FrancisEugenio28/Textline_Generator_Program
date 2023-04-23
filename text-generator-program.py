bc8567a8139e296fff85e1fa58998ab297ff11d6
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-04-24 01:52:50 +0800

    Pseudocode

---
+++ text-generator-program.py
@@ -0,0 +1,7 @@
+#Francis Niño A. Eugenio
+#BSCPE 1-5 | Object Oriented Programing
+#Textline Generator
+
+#open the source file
+#create an input for th user 
+#ask the user if he/she want to add a line
