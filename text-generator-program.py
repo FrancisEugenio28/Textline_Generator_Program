5884578ddf0c72cfd9ad43a6606ff1ff4910fca3
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-04-24 02:02:51 +0800

    The code is running but there is an Error in line 11. Must fix.

--- text-generator-program.py
+++ text-generator-program.py
@@ -4,9 +4,10 @@
 
 #open the source file
 with open("myLife.txt", "w") as life_file:
-    #create an input for th user 
-    line_input = input("Enter a line: ")
-    #ask the user if he/she want to add a line
-    retry = input("Do you want to add a line? (y/n): ")
-    if retry != "Y":
-        breaks
+    while True:
+        #create an input for th user 
+        line_input = input("Enter a line: ")
+        #ask the user if he/she want to add a line
+        retry = input("Do you want to add a line? (y/n): ")
+        if retry != "Y":
+            break
