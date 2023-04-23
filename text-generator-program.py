9d05e13ffe34e83606a5026f1a03ec0962f8e207
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-04-24 02:00:24 +0800

    In this set of line, we are done making a set of code wherein we ask the user for adding another line. Unfortunately error found (SyntaxError: 'break' outside loop). Must fix.

--- text-generator-program.py
+++ text-generator-program.py
@@ -4,6 +4,9 @@
 
 #open the source file
 with open("myLife.txt", "w") as life_file:
-#create an input for th user 
+    #create an input for th user 
     line_input = input("Enter a line: ")
-#ask the user if he/she want to add a line
+    #ask the user if he/she want to add a line
+    retry = input("Do you want to add a line? (y/n): ")
+    if retry != "Y":
+        breaks
