a7a80b2562c537f5bf61eab153d71cb7c17b95a9
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-04-24 02:13:55 +0800

    I just add some colors in the text for asthetic

--- text-generator-program.py
+++ text-generator-program.py
@@ -2,18 +2,22 @@
 #BSCPE 1-5 | Object Oriented Programing
 #Textline Generator
 
+import colorama
+
+colorama.init()
+
 #open the source file
 with open("myLife.txt", "w") as life_file:
     while True:
         #create an input for th user 
-        line_input = input("Enter a line: ")
+        line_input = input(colorama.Fore.LIGHTCYAN_EX + "Enter a line: ")
         #add a import time for loading effect
         import time
-        print("Printing your entered line in the txt file...")
+        print(colorama.Back.LIGHTWHITE_EX + "Printing your entered line in the txt file...")
         time.sleep(1)
-        print("DONE!")
+        print(colorama.Back.LIGHTWHITE_EX + "DONE!")
         life_file.write(line_input + "\n")
         #ask the user if he/she want to add a line
-        retry = input("Do you want to add a line? (y/n): ").upper()
+        retry = input(colorama.Back.RESET + "Do you want to add a line? (y/n): ").upper()
         if retry != "Y":
             break
